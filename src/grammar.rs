d3cdb39b1cbc9dcac9303064eb75b8a88ea24a88
Devin Yeung <devin.cg.yeung.dev@outlook.com>
2024-09-03 22:54:56 +0800

    checkpoint

--- src/grammar.rs
+++ src/grammar.rs
@@ -1,3 +1,8 @@
+use rand::distributions::Distribution;
+use rand::distributions::WeightedIndex;
+use rand::Rng;
+use std::collections::HashMap;
+
 use crate::{lexer, parser};
 
 #[repr(transparent)]
@@ -10,14 +15,68 @@ impl RawGrammar {
     pub fn parse<S: AsRef<str>>(input: S) -> RawGrammar {
         let lexer = lexer::Lexer::new(input.as_ref());
         let parser = parser::RawGrammarParser::new();
-        parser.parse(lexer).unwrap() // TODO: Error Handling
+        match parser.parse(lexer) {
+            Ok(grammar) => grammar,
+            Err(e) => match e {
+                lalrpop_util::ParseError::UnrecognizedToken { token, expected } => {
+                    panic!("Unrecognized token: {:?}, expected: {:?}", token, expected)
+                }
+                lalrpop_util::ParseError::ExtraToken { token } => {
+                    panic!("Extra token: {:?}", token)
+                }
+                lalrpop_util::ParseError::User { error } => panic!("User error: {:?}", error),
+                lalrpop_util::ParseError::InvalidToken { location } => {
+                    panic!("Invalid token: {:?}", location)
+                }
+                lalrpop_util::ParseError::UnrecognizedEof { location, expected } => {
+                    panic!("Unrecognized EOF: {:?}, expected: {:?}", location, expected)
+                }
+            },
+        } // TODO: Error Handling
+    }
+}
+
+pub struct CheckedGrammar {
+    pub(crate) rules: HashMap<String, WeightedProduction>,
+}
+
+impl CheckedGrammar {
+    fn reduce(&self, symbol: SymbolKind) -> (Option<String>, Vec<SymbolKind>) {
+        match symbol {
+            SymbolKind::Terminal(s) => (Some(s), vec![]),
+            SymbolKind::NonTerminal(s) => {
+                let syms = self.rules.get(&s).unwrap().choose(&mut rand::thread_rng());
+                (None, syms)
+            }
+            SymbolKind::Repeat { symbol, min, max } => {
+                todo!()
+            }
+        }
     }
 }
 
 #[derive(Debug)]
 pub struct Rule {
     pub(crate) name: String,
-    pub(crate) alternatives: Vec<Alternative>,
+    pub(crate) production: WeightedProduction,
+}
+
+#[derive(Debug)]
+#[repr(transparent)]
+pub struct WeightedProduction {
+    pub(crate) production: Vec<Alternative>,
+}
+
+impl WeightedProduction {
+    pub fn choose<R: Rng>(&self, rng: &mut R) -> Vec<SymbolKind> {
+        let dist = WeightedIndex::new(self.production.iter().map(|a| a.weight)).unwrap();
+        let idx = dist.sample(rng);
+        self.production[idx]
+            .symbols
+            .iter()
+            .map(|s| s.kind.clone())
+            .collect()
+    }
 }
 
 #[derive(Debug)]
@@ -26,12 +85,12 @@ pub struct Alternative {
     pub(crate) symbols: Vec<Symbol>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum SymbolKind {
     Terminal(String),
     NonTerminal(String),
     Repeat {
-        symbol: Box<Symbol>,
+        symbol: Box<SymbolKind>,
         min: usize,
         max: Option<usize>,
     },
