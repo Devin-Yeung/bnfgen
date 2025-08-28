b10931cfae6be8d2b94b02a444c617b6943e1820
Devin-Yeung <53309384+Devin-Yeung@users.noreply.github.com>
2025-08-29 01:44:48 +0800

    fix: make sure all required source files are included

--- nix/mkFormula.nix
+++ nix/mkFormula.nix
@@ -2,8 +2,24 @@
   pkgs,
   craneLib,
 }:
+let
+  inherit (pkgs) lib;
+  lalrpopFilter = path: _type: builtins.match ".*lalrpop$" path != null;
+  bnfgenFilter = path: _type: builtins.match ".*bnfgen$" path != null;
+  snapshotFilter = path: _type: builtins.match ".*snap$" path != null;
+  filter =
+    path: type:
+    (craneLib.filterCargoSources path type)
+    || (lalrpopFilter path type)
+    || (bnfgenFilter path type)
+    || (snapshotFilter path type);
+in
 {
-  src = craneLib.cleanCargoSource ../.;
+  src = lib.cleanSourceWith {
+    src = ../.; # The original, unfiltered source
+    name = "source"; # Be reproducible, regardless of the directory name
+    inherit filter;
+  };
   strictDeps = true;
   buildInputs = [
     # Add extra build inputs if needed
