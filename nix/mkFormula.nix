0e9df7d2542139e5e09dbca767ef6b8be9a60006
Devin-Yeung <53309384+Devin-Yeung@users.noreply.github.com>
2025-09-09 01:10:02 +0800

    refactor: a more elegant way to filter source files

--- nix/mkFormula.nix
+++ nix/mkFormula.nix
@@ -4,22 +4,24 @@
 }:
 let
   inherit (pkgs) lib;
-  lalrpopFilter = path: _type: builtins.match ".*lalrpop$" path != null;
-  bnfgenFilter = path: _type: builtins.match ".*bnfgen$" path != null;
-  snapshotFilter = path: _type: builtins.match ".*snap$" path != null;
-  filter =
-    path: type:
-    (craneLib.filterCargoSources path type)
-    || (lalrpopFilter path type)
-    || (bnfgenFilter path type)
-    || (snapshotFilter path type);
+
+  unfilteredRoot = ../.; # The original, unfiltered source
+  src = lib.fileset.toSource {
+    root = unfilteredRoot;
+    fileset = lib.fileset.unions [
+      # Default files from crane (Rust and cargo files)
+      (craneLib.fileset.commonCargoSources unfilteredRoot)
+      # Keep any snapshots files
+      (lib.fileset.fileFilter (file: file.hasExt "snap") unfilteredRoot)
+      # Keep any lalrpop grammar
+      (lib.fileset.fileFilter (file: file.hasExt "lalrpop") unfilteredRoot)
+      # Folder that stores all the bnfgen example
+      (lib.fileset.maybeMissing ../examples)
+    ];
+  };
 in
 {
-  src = lib.cleanSourceWith {
-    src = ../.; # The original, unfiltered source
-    name = "source"; # Be reproducible, regardless of the directory name
-    inherit filter;
-  };
+  inherit src;
   strictDeps = true;
   buildInputs = [
     # Add extra build inputs if needed
