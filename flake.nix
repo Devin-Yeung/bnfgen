9bd6b809615d2c940c545e2a4d39409f3513d08a
Devin-Yeung <53309384+Devin-Yeung@users.noreply.github.com>
2026-02-10 18:24:57 +0800

    refactor: simplify nix build

--- flake.nix
+++ flake.nix
@@ -1,30 +1,97 @@
 {
-  description = "Building static binaries with musl";
+  description = "Bnfgen: A highly customizable BNF based fuzzy tests generator";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     crane.url = "github:ipetkov/crane";
     flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
   outputs =
-    inputs@{ flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
-      imports = [
-        ./flakes/build-static.nix
-        ./flakes/build.nix
-        ./flakes/checks.nix
-      ];
-    };
+    {
+      nixpkgs,
+      crane,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        craneLib = crane.mkLib pkgs;
+        inherit (pkgs) lib;
+
+        unfilteredRoot = ./.;
+        src = lib.fileset.toSource {
+          root = unfilteredRoot;
+          fileset = lib.fileset.unions [
+            (craneLib.fileset.commonCargoSources unfilteredRoot)
+            (lib.fileset.fileFilter (file: file.hasExt "snap") unfilteredRoot)
+            (lib.fileset.fileFilter (file: file.hasExt "lalrpop") unfilteredRoot)
+            (lib.fileset.maybeMissing ./examples)
+          ];
+        };
+
+        commonArgs = {
+          inherit src;
+          strictDeps = true;
+          buildInputs = lib.optionals pkgs.stdenv.isDarwin [
+            pkgs.libiconv
+          ];
+        };
+
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+        bnfgen = craneLib.buildPackage (
+          commonArgs
+          // {
+            inherit cargoArtifacts;
+          }
+        );
+      in
+      {
+        checks = {
+          inherit bnfgen;
+
+          bnfgen-clippy = craneLib.cargoClippy (
+            commonArgs
+            // {
+              inherit cargoArtifacts;
+              cargoClippyExtraArgs = "--all-targets -- --deny warnings";
+            }
+          );
+
+          bnfgen-doc = craneLib.cargoDoc (
+            commonArgs
+            // {
+              inherit cargoArtifacts;
+              env.RUSTDOCFLAGS = "--deny warnings";
+            }
+          );
+
+          bnfgen-fmt = craneLib.cargoFmt {
+            inherit src;
+          };
+
+          bnfgen-toml-fmt = craneLib.taploFmt {
+            src = lib.sources.sourceFilesBySuffices src [ ".toml" ];
+          };
+
+          bnfgen-nextest = craneLib.cargoNextest (
+            commonArgs
+            // {
+              inherit cargoArtifacts;
+              partitions = 1;
+              partitionType = "count";
+              cargoNextestPartitionsExtraArgs = "--no-tests=pass";
+            }
+          );
+        };
+
+        packages = {
+          inherit bnfgen;
+          default = bnfgen;
+        };
+      }
+    );
 }
